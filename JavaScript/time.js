ec05625f9003a95e70b4dc5f92bcaff5bbc9d8fc
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2021-05-02 13:24:44 +0100

    Changing div positions
    Adjusting the sizing and positioning of the divs

--- JavaScript/time.js
+++ JavaScript/time.js
@@ -2,12 +2,24 @@ const timeEl = document.querySelector("#time");
 
 const now = new Date();
 const hour = now.getHours();
-console.log(now);
+const dayNum = now.getDay();
+const dayArr = new Array(7);
 
-console.log(timeEl);
+dayArr[0] = "Sunday";
+dayArr[1] = "Monday";
+dayArr[2] = "Tuesday";
+dayArr[3] = "Wednesday";
+dayArr[4] = "Thursday";
+dayArr[5] = "Friday";
+dayArr[6] = "Saturday";
+
+const day = dayArr[dayNum];
+const greeting = `hope you're having a great ${day} so far!`;
+
+const outtro = `hope you're having a great ${day}`;
 
 if (hour < 12) {
-  timeEl.innerText = "Good Morning";
+  timeEl.innerText = "Good Morning,";
 } else if (hour <= 17 && hour > 12) {
-  timeEl.innerText = "Good Afternoon";
-} else timeEl.innerText = "Good Evening";
+  timeEl.innerText = `Good Afternoon, ${greeting}`;
+} else timeEl.innerText = "Good Evening,";
